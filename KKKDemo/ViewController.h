205ee926be6bd9a49e1221d733fb7f615487a902
Fangfang Zhang <zhangff@ibobei.cn>
2018-10-09 02:48:17 +0800

    添加项目工程

---
+++ KKKDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KKKDemo
+//
+//  Created by Fangfang Zhang on 2018/10/8.
+//  Copyright © 2018年 Fangfang Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KKKDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KKKDemo
+//
+//  Created by Fangfang Zhang on 2018/10/8.
+//  Copyright © 2018年 Fangfang Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
